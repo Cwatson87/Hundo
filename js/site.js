a3717bfc5ae5bdb30dd66852cfe7031ed6eee7b3
Cwatson87 <snivley12@gmail.com>
2021-10-05 12:28:33 -0400

    Spelling...really?

--- js/site.js
+++ js/site.js
@@ -1,29 +1,72 @@
-
 // displays a message to the user
-function displayMessage(){
-    //declare out variable
-    let msg = "" ;
+function displayHundo() {
 
-    //pull the value from the input box
-    msg = document.getElementById("message").value;
-    
-    //altert the user
-    alert(msg);
+    let startValue = 0;
+    let endValue = 100;
+
+    //get numbers from UI
+    startValue = document.getElementById("startValue").value;
+    endValue = document.getElementById("endValue").value;
+
+    //Validate our numbers- start value and end value are integers/whole numbers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //check to see if start and end values are numbers
+    //use case
+    //33 and Davis - True and False = False
+    // 0 and 100 - True and True = True
+    // Bobby and 100 - False and True = False
+    //maybe limit start value and end value?
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //both numbers are intergers
+
+        //get the numbers from the start value to the end value
+        let numbers = generateNumbers(startValue, endValue);
+
+        //display the numbers on the page
+        displayNumbers(numbers)
+
+    } else {
+        //One or more are not intergers
+    }
+
 }
-   
-function displayMessage2() {
-    //declare out variable
-    let msg = "";
 
-    //pull the value from the input box
-    msg = document.getElementById("message").value;
+//generate numbers from startValue to endValue
+function generateNumbers(startValue, endValue) {
 
-    //sweet alert - Alerts the user
-    ;Swal.fire(msg);
+    //0,1,2,3,4... an Array - a list of things seperated by commas
+    let numbers = [];
+    //loop over the numbers until we hit the end value by adding one (++) everytime
+    for (let index = startValue; index <= endValue; index++) {
+
+        //add each number to the array
+        numbers.push(index);
+
+    }
+    return numbers;
 }
 
-    function getValues(){
-        let msg= "";
+//display the nubmers to the page
+function displayNumbers(numbers) {
 
-        msg= document.getElementById("message").value
+    //0 first index value
+    //99 last index value
+    //length = 100
+
+    //0,1,2,3,4,5,6
+    let startValue = numbers[0];
+    let endIndex = numbers.length; //=100
+
+    let ptag = document.getElementById("results")
+
+    for (let index = 0; index < numbers.length; index++) {
+
+        //get the actual number
+        let number = numbers[index];
+        ptag.innerHTML += number + " | ";
+
+    }
 }
